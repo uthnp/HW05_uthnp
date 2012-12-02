94456963faf250b0c4082fd5b61e5a5d80360bac
Nick <uthnp@muohio.edu>
2012-12-02 06:42:54 -0500

    wrote up the GraphMatrix class (untested)

--- ListGraph.cpp
+++ ListGraph.cpp
@@ -1,1 +1,31 @@
 #include "ListGraph.h"
+
+ListGraph::ListGraph(int numNodes)
+{
+	// single vector that points to lists w/ respect to each node
+	// lists contain edge info for that particular node:
+		// which node the edge connects to
+		// the weight of the edge
+
+	//edgeList = 
+}
+
+EdgeWeight ListGraph::weight(NodeID u, NodeID v) const
+{
+}
+
+std::list<NWPair> ListGraph::getAdj(NodeID u) const
+{
+}
+
+unsigned ListGraph::degree(NodeID u)const
+{
+}
+
+unsigned ListGraph::size() const
+{
+}
+
+unsigned ListGraph::numEdges() const
+{
+}
--- MatrixGraph.cpp
+++ MatrixGraph.cpp
@@ -1,1 +1,69 @@
 #include "MatrixGraph.h"
+#include <vector>
+
+
+ MatrixGraph::MatrixGraph(unsigned num_nodes)
+ {
+	 //num nodes = length of 2D array's sides... use 1D array, but it wraps
+	 //each space in the array refers to the weight of the edge between node x and y
+	 // if there is no edge between the two, put -1
+
+	 //create a vector for each node, set all it's internal values to -1 and put it into M
+	 std::vector<EdgeWeight>* temp;
+
+	 std::vector<EdgeWeight>* temp;
+	 for (int i = 0; i < num_nodes; i++)
+	 {
+		temp = new std::vector<EdgeWeight>(num_nodes, -1);
+		this->M.at(i) = *temp;
+	 }
+
+	 //initialize the number of edges to 0 so we can easily increment it later
+	 this->num_edges = 0;
+ }
+
+ void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight)
+ {
+	 (M.at(u)).at(v) = weight;
+	 (M.at(v)).at(u) = weight; //since it's a symetric matrix
+	 this->num_edges++;
+ }
+
+ EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const
+ {
+	 return (M.at(u)).at(v);
+ }
+
+ std::list<NWPair> MatrixGraph::getAdj(NodeID u) const
+ {
+	 //return a list of pairs (node# and weight of edge between it and u)
+	 std::list<NWPair> results;
+	 NWPair temp;
+	 for (int i = 0; i < M.size(); i++)
+	 {
+		 temp = NWPair(i, (M.at(u)).at(i));
+		 results.push_back(temp);
+	 }
+	 return results;
+ }
+
+ unsigned MatrixGraph::degree(NodeID u) const
+ {
+	 //gets the number of edges that u shares with other nodes
+	 int numAdj = 0;
+	 for (int i = 0; i < M.size(); i++)
+	 {
+		 if ((M.at(u)).at(i) > -1) { numAdj++; }
+	 }
+	 return numAdj;
+ }
+
+ unsigned MatrixGraph::size() const
+ {
+	 return M.size();
+ }
+
+ unsigned MatrixGraph::numEdges() const
+ {
+	 return this->num_edges;
+ }
